ffb3fb32517df521a98141235b653abff0ebf7a5
Yuri Akira <yuriakirashiba@gmail.com>
2022-11-14 22:38:57 -0300

    [project2/exercise3] add source code

---
+++ entrega2/atividade3/bridge-java/ex3/TrafficController.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.*;
+
+public class TrafficController {
+    public void enterLeft() { }
+    public void enterRight() { }
+    public void leaveLeft() { }
+    public void leaveRight() { }
+
+}
